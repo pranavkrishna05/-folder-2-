a52068eaa42a1a9116598694d0122034b2d6cee3
pranavkrishna05 <pranav2002krishna@gmail.com>
2025-12-18 12:23:30 +0530

    Delete utils directory

--- utils/validateEmail.js
+++
@@ -1,6 +0,0 @@
-const validateEmail = (email) => {
-    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    return emailRegex.test(email);
-};
-
-module.exports = validateEmail;
--- utils/validatePassword.js
+++
@@ -1,6 +0,0 @@
-const validatePassword = (password) => {
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
-    return passwordRegex.test(password);
-};
-
-module.exports = validatePassword;
