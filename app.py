be223d079e329bb9f98866d4f93872efc9dfeac4
pranavkrishna05 <pranav2002krishna@gmail.com>
2025-12-18 15:19:06 +0530

    Create or update app.py

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.user_account.interfaces.routes.user_routes import user_bp
 import logging
+from datetime import timedelta
 
 # Initialize the Flask application
 app = Flask(__name__)
 app.config.from_object('config.Config')
+app.secret_key = app.config['SECRET_KEY']
+app.permanent_session_lifetime = timedelta(minutes=30)
 
 # Set up structured logging
 logging.basicConfig(level=logging.INFO)
