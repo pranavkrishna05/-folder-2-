16eced654bd762b9e8b65b85a7f9047507ffb7d3
pranavkrishna05 <pranav2002krishna@gmail.com>
2025-12-18 15:04:54 +0530

    Creating user.py to define User entity

---
+++ app/user_account/domain/entities/user.py
@@ -0,0 +1,7 @@
+from app.database import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
