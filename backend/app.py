c7231d2419b5be401ac31eae69e73d924afea976
pranavkrishna05 <pranav2002krishna@gmail.com>
2025-12-18 14:54:42 +0530

    Updating or creating backend/app.py

--- backend/app.py
+++ backend/app.py
@@ -17,4 +17,4 @@ def create_app() -> Flask:
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
