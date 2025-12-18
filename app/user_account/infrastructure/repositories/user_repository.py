45aec2a91b85714115aef11921d46d1206ecce5d
pranavkrishna05 <pranav2002krishna@gmail.com>
2025-12-18 15:19:19 +0530

    Adding user repository

--- app/user_account/infrastructure/repositories/user_repository.py
+++ app/user_account/infrastructure/repositories/user_repository.py
@@ -1,13 +1,17 @@
 from app.database import db
 from app.user_account.domain.entities.user import User
+import logging
+
+logger = logging.getLogger(__name__)
 
 class UserRepository:
     @staticmethod
-    def add_user(email: str, password: str) -> None:
-        new_user = User(email=email, password=password)
-        db.session.add(new_user)
-        db.session.commit()
-
-    @staticmethod
     def find_by_email(email: str) -> User:
         return User.query.filter_by(email=email).first()
+
+    @staticmethod
+    def update_password(user_id: int, new_password: str) -> None:
+        user = User.query.get(user_id)
+        if user:
+            user.set_password(new_password)
+            db.session.commit()
