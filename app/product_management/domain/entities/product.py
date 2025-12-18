87808eb0736ffe7ac5109a5c86de3906b90f6b92
pranavkrishna05 <pranav2002krishna@gmail.com>
2025-12-18 15:19:36 +0530

    Add product entity for product management

--- app/product_management/domain/entities/product.py
+++ app/product_management/domain/entities/product.py
@@ -3,6 +3,7 @@ from app.database import db
 class Product(db.Model):
     __tablename__ = 'products'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
     price = db.Column(db.Float, nullable=False)
-    description = db.Column(db.String(500), nullable=True)
+    description = db.Column(db.Text, nullable=False)
+    category = db.Column(db.String(120), nullable=True)
