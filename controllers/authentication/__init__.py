b44ef862135fa2708fd53c78280acff1ce7cb62c
pranavkrishna05 <pranav2002krishna@gmail.com>
2025-12-19 18:47:22 +0530

    Initial commit

--- controllers/authentication/__init__.py
+++ controllers/authentication/__init__.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 auth_bp = Blueprint('auth', __name__)
 
-@auth_bp.route('/login')
+@auth_bp.route('/login', methods=['POST'])
 def login():
-    return 'Login Page'
+    return 'Login endpoint'
