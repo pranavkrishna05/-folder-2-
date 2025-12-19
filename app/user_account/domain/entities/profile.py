0cf8f09feb0e84299f1ed0404795cb34f11b15ab
pranavkrishna05 <pranav2002krishna@gmail.com>
2025-12-19 14:28:26 +0530

    Delete app directory

--- app/user_account/domain/entities/profile.py
+++
@@ -1,9 +0,0 @@
-from app.database import db
-
-class Profile(db.Model):
-    __tablename__ = 'profiles'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, unique=True, nullable=False)
-    first_name = db.Column(db.String(50))
-    last_name = db.Column(db.String(50))
-    preferences = db.Column(db.JSON)
--- app/user_account/services/profile_service.py
+++
@@ -1,6 +0,0 @@
-from app.user_account.infrastructure.repositories.profile_repository import ProfileRepository
-
-class ProfileService:
-    @staticmethod
-    def update_profile(user_id: int, data: dict) -> None:
-        ProfileRepository.update_profile(user_id, data)
