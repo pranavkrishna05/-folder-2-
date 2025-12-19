529bcf4494de4350454cbc763d11f5b33f7627d7
pranavkrishna05 <pranav2002krishna@gmail.com>
2025-12-19 07:13:00 +0530

    Create or update file: app/app.py

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, session
 from user_account.interfaces.routes.auth import bp as auth_bp
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
+app.config['SESSION_COOKIE_NAME'] = 'my_session'
 app.register_blueprint(auth_bp)
 
 if __name__ == '__main__':
