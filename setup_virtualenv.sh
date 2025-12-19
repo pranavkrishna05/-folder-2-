4a0b94edbf919f3072bc51d9646ef4756509896b
pranavkrishna05 <pranav2002krishna@gmail.com>
2025-12-19 16:25:32 +0530

    Add file setup_virtualenv.sh

---
+++ setup_virtualenv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create a virtual environment
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install required packages
+pip install -r requirements.txt
