71dd805ee337021e7abd98382d28bd4b5021e68c
pranavkrishna05 <pranav2002krishna@gmail.com>
2025-12-19 16:26:33 +0530

    Update config.py with new content

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 import os
 
 class Config:
@@ -7,4 +6,4 @@ class Config:
     """
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
     DEBUG = os.environ.get('DEBUG', False)
-    DATABASE_URI = os.environ.get('DATABASE_URI', 'sqlite:///:memory:')
+    DATABASE_URI = os.environ.get('DATABASE_URI', 'mysql+pymysql://username:password@localhost/dbname')
