0c3547a1cb2d8e67526cd05ef90a58548fd7f838
pranavkrishna05 <pranav2002krishna@gmail.com>
2025-12-20 16:57:32 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    """
-    Base configuration class.
-    """
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
-    DEBUG = os.environ.get('DEBUG', False)
-    DATABASE_URI = os.environ.get('DATABASE_URI', 'mysql+pymysql://username:password@localhost/dbname')
