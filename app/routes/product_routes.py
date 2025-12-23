da01518c55bf8491f21980f538b4430188d064ca
pranavkrishna05 <pranav2002krishna@gmail.com>
2025-12-23 09:22:51 +0530

    this message is  committed by n8n 2.0

---
+++ app/routes/product_routes.py
@@ -0,0 +1,11 @@
+"""
+Product-related API routes.
+"""
+
+from flask import Blueprint
+from app.controllers.product_controller import product_blueprint
+
+product_routes = Blueprint("product_routes", __name__, url_prefix="/products")
+
+# Register blueprints
+product_routes.register_blueprint(product_blueprint)
