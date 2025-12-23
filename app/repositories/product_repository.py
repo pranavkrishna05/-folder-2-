beeeb31e433949684c8bcce198ab533a90292484
pranavkrishna05 <pranav2002krishna@gmail.com>
2025-12-23 09:25:55 +0530

    commiuted

--- app/repositories/product_repository.py
+++ app/repositories/product_repository.py
@@ -1,30 +1,22 @@
 """
-Repository layer for product data interactions, including search functionality.
+Repository layer for product data interactions.
 """
 
 from app.models.product import Product, db
-from sqlalchemy.orm import Query
 
 
 class ProductRepository:
     """Provides Product model-related database functionality."""
 
     @staticmethod
-    def search_products(
-        keyword: str, category: str = None, page: int = 1, per_page: int = 10
-    ) -> dict:
-        """Search products based on keyword and category."""
-        query: Query = Product.query.filter(
-            Product.name.ilike(f"%{keyword}%") | Product.description.ilike(f"%{keyword}%")
-        )
-        
-        if category:
-            query = query.filter(Product.category_name.ilike(f"%{category}%"))
-        
-        paginated_results = query.paginate(page=page, per_page=per_page, error_out=False)
-        return {
-            "total": paginated_results.total,
-            "pages": paginated_results.pages,
-            "current_page": paginated_results.page,
-            "products": [product.to_dict() for product in paginated_results.items],
-        }
+    def get_product_by_id(product_id: int) -> Product | None:
+        """Retrieve product by its ID."""
+        return Product.query.get(product_id)
+
+    @staticmethod
+    def update_product(product_id: int, price: float = None, description: str = None) -> None:
+        """Update an existing product."""
+        product = ProductRepository.get_product_by_id(product_id)
+        if product:
+            product.update_product(price=price, description=description)
+            db.session.commit()
