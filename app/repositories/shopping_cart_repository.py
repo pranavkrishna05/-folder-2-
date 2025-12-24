d2c089e774c7a1ffc292ffd81871a09969fc9d8c
pranavkrishna05 <pranav2002krishna@gmail.com>
2025-12-24 17:33:27 +0530

    Enhance shopping_cart_repository.py with additional functionality and clarity

--- app/repositories/shopping_cart_repository.py
+++ app/repositories/shopping_cart_repository.py
@@ -4,6 +4,7 @@ Repository layer for shopping cart data interactions, enhancing persistence acro
 
 from app.models.shopping_cart import ShoppingCart, CartItem, db
 
+
 class ShoppingCartRepository:
     """Provides ShoppingCart model-related database functionality."""
 
@@ -30,8 +31,10 @@ class ShoppingCartRepository:
         item = CartItem(product_id=product_id, quantity=quantity, shopping_cart_id=cart_id)
         db.session.add(item)
         db.session.commit()
+
         cart = ShoppingCart.query.get(cart_id)
         cart.update_total_price()
+
         return item
 
     @staticmethod
@@ -39,3 +42,46 @@ class ShoppingCartRepository:
         """Retrieve all items in a shopping cart."""
         cart = ShoppingCart.query.get(cart_id)
         return cart.cart_items if cart else []
+
+    @staticmethod
+    def remove_item_from_cart(cart_id: int, item_id: int) -> bool:
+        """Remove an item from the shopping cart."""
+        cart = ShoppingCart.query.get(cart_id)
+        if not cart:
+            return False
+
+        item = next((i for i in cart.cart_items if i.id == item_id), None)
+        if item:
+            db.session.delete(item)
+            db.session.commit()
+            cart.update_total_price()
+            return True
+        return False
+
+    @staticmethod
+    def clear_cart(cart_id: int) -> bool:
+        """Clear all items from a shopping cart."""
+        cart = ShoppingCart.query.get(cart_id)
+        if not cart:
+            return False
+
+        for item in cart.cart_items:
+            db.session.delete(item)
+        db.session.commit()
+        cart.update_total_price()
+        return True
+
+    @staticmethod
+    def update_item_quantity(cart_id: int, item_id: int, quantity: int) -> bool:
+        """Update the quantity of an item in the cart."""
+        cart = ShoppingCart.query.get(cart_id)
+        if not cart:
+            return False
+
+        item = next((i for i in cart.cart_items if i.id == item_id), None)
+        if item:
+            item.quantity = quantity
+            db.session.commit()
+            cart.update_total_price()
+            return True
+        return False
